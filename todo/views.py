29d042ed4b370b5e0fe9baa652c7e9b5b69079b7
lubnariyaz <71705544+lubnariyaz@users.noreply.github.com>
2024-03-09 16:32:24 +0530

    Commit 5: Mark as Done added

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Task
 
@@ -8,3 +8,8 @@ def addTask(request):
     Task.objects.create(task=task)
     return redirect('home')
 
+def mark_as_done(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
